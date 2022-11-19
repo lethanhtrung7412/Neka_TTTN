7bf2a2745816460ad891c4ef8e282efac461e1e1
ducthuanlk22222 <92450417+ducthuanlk22222@users.noreply.github.com>
2022-11-19 15:26:25 +0700

    fix bug search in home page

--- CosmeticsShop/Controllers/ProductController.cs
+++ CosmeticsShop/Controllers/ProductController.cs
@@ -15,34 +15,23 @@ namespace CosmeticsShop.Controllers
         public ActionResult Index(int CategoryID = 0, string keyword = "", int? page = 1)
         {
             ViewBag.ListCategory = db.Categories.Where(x => x.IsActive == true).ToList();
-            List<Product> products = new List<Product>();
             if (keyword != "")
             {
-                ViewBag.NamePage = "Tìm kiếm sản phẩm";
-                products = db.Products.Where(x => x.IsActive == true && x.Name.Contains(keyword)).ToList();
+                ViewBag.NamePage = "Search product";
+                ViewBag.ListProduct = db.Products.Where(x => x.IsActive == true && x.Name.Contains(keyword)).ToList();
                 return View();
             }
             if (CategoryID != 0)
             {
-                ViewBag.NamePage = "Danh mục " + db.Categories.Find(CategoryID).Name;
-                products = db.Products.Where(x => x.IsActive == true && x.CategoryID == CategoryID).ToList();
+                ViewBag.NamePage = "Category " + db.Categories.Find(CategoryID).Name;
+                ViewBag.ListProduct = db.Products.Where(x => x.IsActive == true && x.CategoryID == CategoryID).ToList();
             }
             else
             {
-                ViewBag.NamePage = "Tất cả sản phẩm";
-                products = db.Products.Where(x => x.IsActive == true).ToList();
+                ViewBag.NamePage = "All products";
+                ViewBag.ListProduct = db.Products.Where(x => x.IsActive == true).ToList();
             }
-
-            //Phân trang
-            if (page == null) page = 1;
-            int pageSize = 5;
-            int pageNumber = (page ?? 1);
-            PagedList<Product> models = new PagedList<Product>(products.AsQueryable(), pageNumber, pageSize);
-
-            //Trang hiện tại
-            ViewBag.ListProduct = products;
-            ViewBag.CurrentPage = pageNumber;
-            return View(models);
+            return View();
         }
         public ActionResult Details(int ID)
         {
